de92373d3e725bf2ade68cab5a347e35685d3ecd
j1t0 <rockypalafle@gmail.com>
2023-06-22 13:00:14 +0200

    Banner terminado. Faltan efectos

--- tuot/src/app/app.module.ts
+++ tuot/src/app/app.module.ts
@@ -3,25 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { BodyComponent } from './shared/body/body.component';
-=======
 import { HeaderComponent } from './share/header/header.component';
 import { FooterComponent } from './share/footer/footer.component';
->>>>>>> 8c3da8daa1097c490ac14a260f98933aedc7a517
+import { BannerComponent } from './share/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-<<<<<<< HEAD
     FooterComponent,
-    BodyComponent
-=======
-    FooterComponent
->>>>>>> 8c3da8daa1097c490ac14a260f98933aedc7a517
+    BannerComponent
   ],
   imports: [
     BrowserModule,
--- tuot/src/app/share/banner/banner.component.spec.ts
+++ tuot/src/app/share/banner/banner.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BodyComponent } from './body.component';
+import { BannerComponent } from './banner.component';
 
-describe('BodyComponent', () => {
-  let component: BodyComponent;
-  let fixture: ComponentFixture<BodyComponent>;
+describe('BannerComponent', () => {
+  let component: BannerComponent;
+  let fixture: ComponentFixture<BannerComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [BodyComponent]
+      declarations: [BannerComponent]
     });
-    fixture = TestBed.createComponent(BodyComponent);
+    fixture = TestBed.createComponent(BannerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
