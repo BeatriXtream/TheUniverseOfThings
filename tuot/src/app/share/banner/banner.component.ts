f7769b4a483235349ce0583b368e2a663a01fd9b
j1t0 <rockypalafle@gmail.com>
2023-06-26 16:38:54 +0200

    Card API version 1

--- tuot/src/app/share/banner/banner.component.ts
+++ tuot/src/app/share/banner/banner.component.ts
@@ -7,15 +7,4 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent {
 
-  // imgBannerLeft = [
-  //   {url: '../../../assets/img/img fondo web/32.png', txtLeft: 'RYU'},
-  //   {url: '../../../assets/img/img fondo web/20.png', txtLeft: 'CAPITAN AMERICA'},
-  //   {url: '../../../assets/img/img fondo web/69.png', txtLeft: 'MAZINGUER Z'},
-  //   ];
-
-  //   imgBannerRight = [
-  //     {url: '../../../assets/img/img fondo web/Ken.png', txtRight: 'KEN'},
-  //     {url: '../../../assets/img/img fondo web/10.png',  txtRight: 'THANOS'},
-  //     {url: '../../../assets/img/img fondo web/68.png',  txtRight: 'CONDE DECAPITADO'},
-  //   ];
 }
---
+++ tuot/src/app/share/body/body.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.scss']
+})
+export class BodyComponent {
+personajes: any;
+
+}
